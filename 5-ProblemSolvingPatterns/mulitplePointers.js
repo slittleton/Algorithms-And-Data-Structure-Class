b2b1e3fca8c1675a83751ad85cd988a7696c22f4
slittleton <littletonltp@gmail.com>
2019-09-02 09:41:33 -0700

    commit

--- 5-ProblemSolvingPatterns/mulitplePointers.js
+++ 5-ProblemSolvingPatterns/mulitplePointers.js
@@ -1,72 +1,61 @@
-// PROBLEM SOLVING PATTERNS
+/**
+ * MULTIPLE POINTERS
+ * 
+ * creating pointers or values that correspond to an index or position
+ * and move towards the beginning, end, or middle based on a certain
+ * condition.
+ * 
+ * very efficient for solving problems with minimal space complexity
+ */
 
-// FREQUENCY COUNTERS =====================
-/* Use object or sets to collect values/frequencies of values*/
+ // EXAMPLE
+ /**
+  * Write a funciton called sumZero which accepts a sorted array of
+  * integers. The function should find the first pair where the
+  * sum is 0. Return an array that includes both values that sum
+  * to zeor or undefined if a pair does not exist
+  */
 
-/*
-///////////////////// EXAMPLE 2 /////////////////////
-Given two strings, write a function to determine if the second string 
-is an anagram of the first. An anagram is a word, phrase, or name formed 
-by rearranging the letters of another, such as cinema, form from iceman
-
-validAnagram('', '') //true
-validAnagram('rat', 'car') //false
-validAnagram('cool', 'coo') //false
-validAnagram('texttwisttime', 'texttwisttime') //true
-
-*/
-
-// ===================== solution - O(n) =====================
-
-function validAnagram(str1, str2){
-  const arr1 = str1.split('');
-  const arr2 = str2.split('');
-  const obj1 = {};
-  const obj2 = {};
-  
-  for(let val of arr1){
-    obj1[val] = (obj1[val] || 0) + 1;
-  }
-  for(let val of arr2){
-    obj2[val] = (obj2[val] || 0) + 1
-  }
-
-  for(let key in obj1){
-    if(!obj2[key]){ return false}
-    if(obj2[key] !== obj1[key]){return false}
-  }
-  return true;
-  
-  }
-  let x;
-  // x = validAnagram('aaa', 'aaa') // true;
-  // x = validAnagram('aaa', 'aab') // false;
-  // x = validAnagram('aaa', 'aabb') // false;
-  x = validAnagram('cinema', 'iceman') // true;
-  
-  console.log(x);
+  // ============= Naive Solution O(n^2) ===============
 
 
-  // ===================== solution2 - O(n) =====================
-  function validAnagramV2(first, second){
-    if(first.length !== second.length){
-      return false;
+function sumZero (arr){
+
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i+1; j < arr.length; j++){
+      if(arr[i] + arr[j] === 0){
+        return[ arr[i], arr[j]]
+      }
     }
-    const lookup ={}
-
-    for(let i = 0; i< first.length;i++){
-      let letter = first[i];
-      //if letter exists, increment, otherwise set to 1
-      lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
-    }
-
-    for(let i = 0; i< second.length; i++){
-      let letter = second[i];
-      // can't find letter or letter is zero then its not an anagram
-      if(!lookup[letter]){ return false }
-      else { lookup[letter] -= 1;}
-    }
-    return true
   }
-  let y = validAnagramV2('cinema', 'iceman')
-  console.log(y);
+}
+
+// let x;
+// x = sumZero([-3,-2,-1,0,1,2,3]) //[-3,3]
+// // x = sumZero([-2,-1,0,3]) //undefined
+// // x = sumZero([1,2,3]) //undefined
+
+// console.log(x);
+
+// =================== Refactor O(n) ===================
+function sumZeroV2 (arr){
+let left = 0;
+let right = arr.length-1;
+
+  while(left < right){
+    let sum = arr[left] + arr[right];
+    if(sum===0){
+      return[arr[left], arr[right]]
+    }else if(sum > 0){
+      right--;
+    }else{
+      left++
+    }
+  }
+}
+let y;
+y = sumZero([-3,-2,-1,0,1,2,3]) //[-3,3]
+// y = sumZero([-2,-1,0,3]) //undefined
+// y = sumZero([1,2,3]) //undefined
+
+console.log(y);
